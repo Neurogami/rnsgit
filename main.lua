de8677706f5fa88575baf91262c6e77161cfc668
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2015-07-14 22:06:18 +0200

    Added notifier for app_new_document

--- main.lua
+++ main.lua
@@ -12,10 +12,25 @@ local options = renoise.Document.create("RnsGitPreferences") {
 
 renoise.tool().preferences = options
 
+-- Invoked each time a new document (song) was created or loaded.
+renoise.tool().app_new_document_observable:add_notifier(function()
+  handle_app_new_document_notification()
+end)
+
+
 -- Invoked each time the apps document (song) was successfully saved.
 renoise.tool().app_saved_document_observable:add_notifier(function()
   handle_app_saved_document_notification()
 end)
+
+
+-- handle_app_new_document_notification
+function handle_app_new_document_notification()
+  if (options.debug.value) then
+    print(("com.renoise.RnsGit: !! app_new_document "..
+      "notification (filename: '%s')"):format(renoise.song().file_name))
+  end
+end
 
 
 -- handle_app_saved_document_notification
